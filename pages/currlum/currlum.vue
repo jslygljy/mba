cb0372e4d8e5cc682c11af06b1f28dd89d65abfd
junyu <liujunyu@qutoutiao.net>
2019-09-18 14:05:39 +0800

    feat: 更新首页逻辑，添加我的课程页面

---
+++ pages/currlum/currlum.vue
@@ -0,0 +1,202 @@
+<template>
+    <view class="content">
+		<sun-tab :value.sync="index" @change="objectChange" :tabList="tabObjectList" rangeKey="name" :scroll="true"></sun-tab>
+		<view class="list-item">
+			<view class="list-item-content" v-for="(item,index) in list" :key="index">
+				<view class="flex-sub">
+					<image :src="item.coverimg" mode=""></image>
+				</view>
+				
+				<view class="item-right flex-treble">
+					<view style="flex:4">
+						<text class="h4">{{item.title}}</text>
+						<text class="grey">主讲:{{item.teacher}}</text>
+					</view>
+					<view class="item-bottom">
+						<text class="grey">{{item.status}}</text>
+					</view>
+				</view>
+			</view>
+		</view>
+    </view>
+</template>
+
+<script>
+ 
+	import sunTab from '@/components/sun-tab/sun-tab.vue';
+	
+    export default {
+		components:{
+			sunTab
+		},
+		data() {
+		    return {
+				index: 0,
+				
+				list:[{
+					'title':'真题课程包',
+					'teacher':'123',
+					'status':'未学习',
+					'id':1,
+					'coverimg':'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+				},{
+					'title':'真题课程包真题课程包真题课程包真题课程包',
+					'teacher':'123',
+					'status':'已学习',
+					'id':2,
+					'coverimg':'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+				},],
+                tabObjectList: [ //对象数组赋值
+                    {
+                        name: '全部',
+                        value: 1
+                    },
+                    {
+                        name: '逻辑',
+                        value: 2
+                    },
+                    {
+                        name: '数学',
+                        value: 3
+                    },
+                    {
+                        name: '英语',
+                        value: 4
+                    },
+                    {
+                        name: '写作',
+                        value: 5
+                    },
+                    {
+                        name: '面试',
+                        value: 6
+                    },
+                    {
+                        name: '助力',
+                        value: 7
+                    }
+                ],
+			}
+		},
+		methods:{
+            objectChange(e){
+                console.log('对象数据返回格式');
+                console.log(e);
+            },
+			
+		}
+		
+    }
+</script>
+
+<style scoped lang="scss">
+	@import "../../static/icon.css";
+	@import "../../static/main.css";
+	.content{
+		padding: 10upx;
+	}
+	.list-item{
+		margin-top: 50rpx;
+		margin-left: 15rpx;
+		font-size: 40rpx;
+		.list-item-content{
+			display: flex;
+			margin-top: 30rpx;
+			image{
+				width: 200rpx;
+				height: 200rpx;
+				border-radius: 10rpx;
+			}
+			.item-right{
+				font-size: 24rpx;
+				margin-left: 40rpx;
+				display: flex;
+				flex-direction: column;
+				margin-right: 20rpx;
+				.h4{
+					font-size: 32rpx;
+					margin-top: 2rpx;
+					display: block;
+					margin-bottom: 10rpx;
+				}
+				.grey{
+					color: #8f8f94
+				}
+				.item-bottom{
+					flex:1
+				}
+				.price{
+					color: #dc143c;
+					font-size: 30rpx;
+				}
+				.sale{
+					margin-left: 20rpx;
+					color: #8f8f94;
+					text-decoration:line-through;
+				}
+				.buy{
+					color: #8f8f94;
+					margin-left: 60rpx;
+				}
+			}
+		}
+	}
+	.list{
+		display: flex;
+		margin-bottom: 20rpx;
+		margin-top: 20rpx;
+		.item{
+			flex:1;
+			text-align: center;
+			font-size: 24rpx;
+		}
+		.i{
+			margin-left: 60rpx;
+			margin-top: 20rpx;
+			margin-bottom: 20rpx;
+		}
+	}
+	.ad{
+		
+		width: 100%;
+		height: 100rpx;
+		margin-bottom: 20rpx;
+		image{
+			margin: 0px 0rpx 0px 10rpx;
+			width: 710rpx;
+			height: 100%;
+		}
+	}
+    .title {
+        color: #8f8f94;
+        margin-top: 50upx;
+    }
+	.icon1{
+		background-image: url('/static/img/bookmark.png');
+		display: block;
+		width: 64upx;
+		height: 64upx;
+		background-size: 100% 100%;
+	}
+	.icon2{
+		background-image: url('/static/img/notebook.png');
+		display: block;
+		width: 64upx;
+		height: 64upx;
+		background-size: 100% 100%;
+	}
+	.icon3{
+		background-image: url('/static/img/report.png');
+		display: block;
+		width: 64upx;
+		height: 64upx;
+		background-size: 100% 100%;
+	}
+	.icon4{
+		background-image: url('/static/img/monitor.png');
+		display: block;
+		width: 64upx;
+		height: 64upx;
+		background-size: 100% 100%;
+	}
+</style>
--- pages/main/main.vue
+++ pages/main/main.vue
@@ -10,7 +10,7 @@
 				<text class="icon2 i"></text>
 				<text>每日阅读</text>
 			</view>
-			<view class="item">
+			<view class="item" @click="goToCurriculum">
 				<text class="icon3 i"></text>
 				<text>我的课程</text>
 			</view>
@@ -23,36 +23,20 @@
 			<image src="https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg" mode=""></image>
 		</view>
 		<sun-tab :value.sync="index" @change="objectChange" :tabList="tabObjectList" rangeKey="name" :scroll="true"></sun-tab>
-		<view class="list-item">
-			<h3>真题课程包</h3>
+		
+		<view class="list-item" v-for="(item,index) in list" :key="index">
+			<h3>{{item.title}}</h3>
 			<view class="list-item-content">
 				<image src="https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg" mode=""></image>
 				<view class="item-right">
 					<view style="flex:4">
-						<h4 class="h4">真题课程包</h4>
-						<text class="grey">提分神奇 商家看过</text>
+						<h4 class="h4">{{item.name}}</h4>
+						<text class="grey">{{item.subinfo}}</text>
 					</view>
 					<view class="item-bottom">
-						<text class="price">$777</text>
-						<text class="sale">$7774</text>
-						<text class="buy">33万人已购</text>
-					</view>
-				</view>
-			</view>
-		</view>
-		<view class="list-item">
-			<h3>真题课程包</h3>
-			<view class="list-item-content">
-				<image src="https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg" mode=""></image>
-				<view class="item-right">
-					<view style="flex:4">
-						<h4 class="h4">真题课程包</h4>
-						<text class="grey">提分神奇 商家看过</text>
-					</view>
-					<view class="item-bottom">
-						<text class="price">$777</text>
-						<text class="sale">$7774</text>
-						<text class="buy">33万人已购</text>
+						<text class="price">{{item.price}}</text>
+						<text class="sale">{{item.sale}}</text>
+						<text class="buy">{{item.buy}}人已购</text>
 					</view>
 				</view>
 			</view>
@@ -77,7 +61,25 @@
 		    return {
 				swiperList:[{img: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'},{img: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'},{img: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'}],
 				index: 0,
-                tabList: ['选项卡一','选项卡二','选项卡三'], //普通数据赋值
+				list:[{
+					'title':'真题课程包',
+					'name':'真题课程包',
+					'subinfo':'提分神奇 商家看过',
+					'price':'$777',
+					'sale':'$7774',
+					'buy':'33万',
+					'id':1,
+					'coverimg':'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+				},{
+					'title':'真题课程包',
+					'name':'真题课程包',
+					'subinfo':'提分神奇 商家看过',
+					'price':'$777',
+					'sale':'$7774',
+					'buy':'33万',
+					'id':2,
+					'coverimg':'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+				},],
                 tabObjectList: [ //对象数组赋值
                     {
                         name: '选项卡一',
@@ -143,6 +145,11 @@
 			goToRead(){
 				 uni.reLaunch({
 				    url: '../read/read',
+				});
+			},
+			goToCurriculum(){
+				uni.reLaunch({
+				    url: '../currlum/currlum',
 				});
 			}
 		}
