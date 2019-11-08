dfcc6b842818687237cf34afd523db8bbf2ab320
[liujunyu@qutoutiao.net] <liujunyu@qutoutiao.net>
2019-11-08 19:20:45 +0800

    feat: 支持全部解析

--- pages/allDetail/allDetail.vue
+++ pages/allDetail/allDetail.vue
@@ -2,48 +2,47 @@
     <view class="errDetail">
 		<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="false" :interval="2000" :duration="500" current="curryIndex" @change="change">
 			<swiper-item v-for="(item, index) in list" :key="index">
-			<view class="header">
-				<text>{{item.ttitle}}</text>
-				<view class="right-progress">
-					<text class="text-blue">{{curryIndex+1}}</text>
-					/
-					<text>{{qa_id.length}}</text>
-				</view>
-			</view>
-			<view class="content">
-				<text>({{item.type==1? '单选题':'多选题'}}){{item.title}}</text>
-			</view>
-			<view class="ans-list">
-				<view class="ans-item" v-for="(subitem, subindex) in item.item_list" :key="subindex">
-					<view class="item-left flex-sub">
-						<view v-if="subitem.option===item.answer && subitem.is_true ==0" class='border-info border-red'>{{subitem.option}}</view>
-						<view v-if="subitem.option!==item.answer && subitem.is_true ==1" class='border-info border-green'>{{subitem.option}}</view>
-						<view v-if="subitem.option!==item.answer && subitem.is_true ==0" class='border-info'>{{subitem.option}}</view>
+				<view class="header">
+					<text>{{item.ttitle}}</text>
+					<view class="right-progress">
+						<text class="text-blue">{{curryIndex+1}}</text>
+						/
+						<text>{{qa_id.length}}</text>
 					</view>
-					<view class="item-right">{{subitem.content}}</view>
 				</view>
-			</view>
+				<view class="content">
+					<text>({{item.type==1? '单选题':'多选题'}}){{item.title}}</text>
+				</view>
+				<view class="ans-list">
+					<view class="ans-item" v-for="(subitem, subindex) in item.item_list" :key="subindex">
+						<view class="item-left flex-sub">
+							<view v-if="subitem.option===item.answer && subitem.is_true ==0" class='border-info border-red'>{{subitem.option}}</view>
+							<view v-if="subitem.option!==item.answer && subitem.is_true ==1" class='border-info border-green'>{{subitem.option}}</view>
+							<view v-if="subitem.option!==item.answer && subitem.is_true ==0" class='border-info'>{{subitem.option}}</view>
+						</view>
+						<view class="item-right">{{subitem.content}}</view>
+					</view>
+				</view>
+				<text class="title">
+					本题出自
+				</text>
+				<text class="info">
+					{{item.ttitle}}
+				</text>
+				<text class="title">
+					考点
+				</text>
+				<text class="info">
+					{{item.special_work}}
+				</text>
+				<text class="title">
+					解析
+				</text>
+				<text class="info">
+					{{item.reason}}
+				</text>
 			</swiper-item>
 		</swiper>
-		<text class="title">
-			本题出自
-		</text>
-		<text class="info">
-			{{item.ttitle}}
-		</text>
-		<text class="title">
-			考点
-		</text>
-		<text class="info">
-			{{item.special_work}}
-		</text>
-		<text class="title">
-			解析
-		</text>
-		<text class="info">
-			{{item.reason}}
-		</text>
-		
 	</view>
 </template>
 
@@ -53,69 +52,79 @@
 		 components: {},
         data() {
 			return {
-				ttitle:'',
 				indicatorDots:false,
 				curryIndex:0,
-				allIndex:5,
-				type:'',
-				items:{},
-				item_list:[],
-				ansList:[],
-				isShow:false,
-				showdetail:false,
-				qa_id:[],
-				answer:''
+				list:[],
+				qa_id:[]
 			}   
          },
 		 onShow(){
-		 	this.getErrDetail();
+		 	
 		 },
 		 onLoad: function (option) { //option为object类型，会序列化上个页面传递的参数
 		    this.qa_id = JSON.parse(option.qa_id);
+			this.qa_id.forEach(data=>{
+				this.list.push({});
+			});
+			this.getErrDetail(0);
 		 },
         methods: {
-			getErrDetail(){
+			getErrDetail(number){
+				console.log(number);
 				let id =uni.getStorageSync('customer_id');
-				console.log(this.qa_id);
 				// 获取做题列表
 				uni.request({
-					url: config.url+'/app/qa/error/detail/'+this.qa_id[0].qa_id ,
+					url: config.url+'/app/qa/error/detail/'+this.qa_id[number].qa_id ,
 				    data: {
 				    },
 				    success: (res) => {
-						this.items = res.data.data;
-						this.list.push(res.data.data);
+						if(number==0){
+							let c = [];
+							c[0] = res.data.data;
+							for(var i = 0;i <(this.qa_id.length -1); i++){
+								c.push({});
+							};
+							this.list = c;
+							console.log(this.list);
+						}else{
+							let c = JSON.parse(JSON.stringify(this.list));
+							c[number] = res.data.data;
+							this.list = c;
+						}
 				    }
 				});
 			},
 			change(e){
-				console.log(2);
-				this.curryIndex = e.detail.current
+				this.curryIndex = e.detail.current;
+				this.getErrDetail(this.curryIndex);
+				
 			}
         }
     }
 </script>
 
 <style scoped lang="scss">
-	uni-page-body{
-		height: 100%;
-		display: flex;
-		flex-direction: column;
-	}
-	uni-swiper {
-	    height: 100%;
-		display: flex;
-		flex-direction: column;
-	}
+	
 	uni-swiper-item{
 		overflow-y: scroll;
 		overflow-x: hidden;
 	}
-	.container{
-	  
+	.swiper {
+		display:flex;
+		flex: 1 1 auto;
 	}
-    .errDetail{
-		width:100%;
+	uni-swiper{
+		height: auto;
+	}
+	uni-swiper .uni-swiper-wrapper{
+		overflow-x: hidden;
+		overflow-y: scroll;
+	}
+	
+	.errDetail{
+		display: flex;
+		flex-direction: column;
+		width: 100%;
 		padding: 0rpx 20rpx;
 		background-color: #fff;
 		.header{
