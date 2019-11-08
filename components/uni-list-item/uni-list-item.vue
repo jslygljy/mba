85c66e03e6a770cff2944243d4b5fdba410d93b9
[liujunyu@qutoutiao.net] <liujunyu@qutoutiao.net>
2019-11-08 14:46:31 +0800

    feat: 还原list

--- components/uni-list-item/uni-list-item.vue
+++ components/uni-list-item/uni-list-item.vue
@@ -4,60 +4,54 @@
     :hover-class="disabled || showSwitch ? '' : 'uni-list-item--hover'"
     class="uni-list-item"
     @click="onClick">
-    <view class="uni-list-item__container">
-      <view
-        v-if="thumb"
-        class="uni-list-item__icon"><image
-          :src="thumb"
-          class="uni-list-item__icon-img" /></view>
-      <view
-        v-else-if="showExtraIcon"
-        class="uni-list-item__icon">
-        <uni-icons
-          :color="extraIcon.color"
-          :size="extraIcon.size"
-          :type="extraIcon.type"
-          class="uni-icon-wrapper" />
-      </view>
-      <view class="uni-list-item__content">
-        <view class="uni-list-item__content-title">{{ title }}</view>
-        <view
-          v-if="note"
-          class="uni-list-item__content-note">{{ note }}</view>
-      </view>
-      <view
-        v-if="showBadge || showArrow || showSwitch"
-        class="uni-list-item__extra">
-        <uni-badge
-          v-if="showBadge"
-          :type="badgeType"
-          :text="badgeText" />
-        <switch
-          v-if="showSwitch"
-          :disabled="disabled"
-          :checked="switchChecked"
-          @change="onSwitchChange" />
-        <uni-icons
-          v-if="showArrow"
-          :size="20"
-          class="uni-icon-wrapper"
-          color="#bbb"
-          type="arrowright" />
-      </view>
-    </view>
+	<view class="flex">
+		<view class="flex-four">
+			<view class="uni-list-item__container">
+			  <view class="uni-list-item__content">
+				  <view class="blueRound"></view>
+				  <view class="uni-list-item__content-title">{{ title }}</view>
+			  </view>
+			</view>
+			<view class="flex margin-top-sm progress">
+				<view class="cu-progress sm round">
+					<view class="bg-blue" :style="[{ width:(curryNum/allNum)}]"></view>
+				</view>
+				<text class="margin-left text-sm text-grey">{{curryNum}}/{{allNum}}</text>
+				<text class="margin-left text-sm text-grey" style="    vertical-align: top;margin-top: -5rpx;min-width: 100px;">正确率:{{(haveSure/allNum * 100).toFixed(2)}}%</text>
+			</view>
+		</view>
+		<view class="flex-sub margin-top">
+			<text class="cuIcon-edit text-grey edit-button"></text>
+		</view>
+	</view>
   </view>
 </template>
 
 <script>
-import uniIcons from '../uni-icons/uni-icons.vue'
+import uniIcon from '../uni-icon/uni-icon.vue'
 import uniBadge from '../uni-badge/uni-badge.vue'
 export default {
   name: 'UniListItem',
   components: {
-    uniIcons,
+    uniIcon,
     uniBadge
   },
   props: {
+	  curryNum: {
+		// 当前阅读数
+		type: [Number, String],
+		default: 0
+	  },
+	  allNum: {
+		// 阅读总数
+		type: [Number, String],
+		default: 100
+	  },
+	  haveSure:{
+		// 正确数
+		type: [Number, String],
+		default: 0  
+	  },
     title: {
       type: String,
       default: ''
@@ -136,34 +130,45 @@ export default {
 }
 </script>
 
-<style lang="scss">
-@mixin list-hover {
-	background-color: $uni-bg-color-hover;
-}
-
+<style lang="scss" scoped>
 @mixin list-disabled {
 	opacity: 0.3;
 }
 
 $list-item-pd: $uni-spacing-col-lg $uni-spacing-row-lg;
-
+.progress{
+	width: 70%;
+	margin-left: 60rpx;
+}
+.blueRound{
+	display: inline-block;
+	width: 15rpx;
+	height: 15rpx;
+	background-color: dodgerblue;
+	vertical-align: top;
+	border-radius: 50%;
+	margin-top: 14rpx;
+	margin-right: 20rpx;
+}
+.edit-button{
+	position: absolute;
+	right: 60rpx;
+    top: 44rpx;
+	font-size: 50rpx;
+}
 .uni-list-item {
 	font-size: $uni-font-size-lg;
 	position: relative;
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
-	align-items: center;
-
-	&--disabled {
+	margin-top: 10rpx;
+	height: 150rpx;
+	.uni-list-item--disabled {
 		@include list-disabled;
 	}
 
-	&--hover {
-		@include list-hover;
-	}
-
-	&__container {
+	.uni-list-item__container {
 		padding: $list-item-pd;
 		width: 100%;
 		box-sizing: border-box;
@@ -173,68 +178,54 @@ $list-item-pd: $uni-spacing-col-lg $uni-spacing-row-lg;
 		flex-direction: row;
 		justify-content: space-between;
 		align-items: center;
-
-		&:after {
-			position: absolute;
-			z-index: 3;
-			right: 0;
-			bottom: 0;
-			left: 30upx;
-			height: 1px;
-			content: '';
-			-webkit-transform: scaleY(0.5);
-			transform: scaleY(0.5);
-			background-color: $uni-border-color;
-		}
 	}
 
-	&__content {
+	.uni-list-item__content {
 		flex: 1;
-		overflow: hidden;
 		display: flex;
-		flex-direction: column;
 		color: #3b4144;
-		&-title {
+		.uni-list-item__content-title {
 			font-size: $uni-font-size-lg;
 			text-overflow: ellipsis;
 			white-space: nowrap;
 			color: inherit;
 			line-height: 1.5;
 			overflow: hidden;
+			display: inline-block;
 		}
 
-		&-note {
-			margin-top: 6upx;
-			color: $uni-text-color-grey;
-			font-size: $uni-font-size-base;
-			white-space: normal;
-			display: -webkit-box;
-			-webkit-box-orient: vertical;
-			-webkit-line-clamp: 2;
-			overflow: hidden;
-		}
+		// &-note {
+		// 	margin-top: 6upx;
+		// 	color: $uni-text-color-grey;
+		// 	font-size: $uni-font-size-base;
+		// 	white-space: normal;
+		// 	display: -webkit-box;
+		// 	-webkit-box-orient: vertical;
+		// 	-webkit-line-clamp: 2;
+		// 	overflow: hidden;
+		// }
 	}
 
-	&__extra {
-		width: 25%;
-		display: flex;
-		flex-direction: row;
-		justify-content: flex-end;
-		align-items: center;
-	}
+	// &__extra {
+	// 	width: 25%;
+	// 	display: flex;
+	// 	flex-direction: row;
+	// 	justify-content: flex-end;
+	// 	align-items: center;
+	// }
 
-	&__icon {
-		margin-right: 18upx;
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
+	// &__icon {
+	// 	margin-right: 18upx;
+	// 	display: flex;
+	// 	flex-direction: row;
+	// 	justify-content: center;
+	// 	align-items: center;
 
-		&-img {
-			height: $uni-img-size-base;
-			width: $uni-img-size-base;
-		}
-	}
+	// 	&-img {
+	// 		height: $uni-img-size-base;
+	// 		width: $uni-img-size-base;
+	// 	}
+	// }
 }
 
 .uni-list > .uni-list-item:last-child .uni-list-item-container:after {
